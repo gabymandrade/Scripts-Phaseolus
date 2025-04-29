3f62d398fc1e28acbdb531c53a2dd9de8a39a075
gabymandrade <59860116+gabymandrade@users.noreply.github.com>
2025-04-29 11:46:29 -0600

    Update 2_ref_gen_download.sh
    add curl

--- 2_ref_gen_download.sh
+++ 2_ref_gen_download.sh
@@ -1,2 +1,10 @@
-Obtenido de Phytozome
+Obtenido de Phytozome:
+Phaseolus coccineus v1.1, https://phytozome.jgi.doe.gov/info/Pcoccineus_v1_1
+Roberto Papa,Elisa Bellucci,Elena Bitocchi,Tania Gioia,Scott Jackson,Jeremy Schmutz
+
+https://data.jgi.doe.gov/refine-download/phytozome?organism=Pcoccineus&_gl=1*rurknw*_ga*OTU0NTEwNjYyLjE3NDMzNjUwNTE.*_ga_YBLMHYR3C2*MTc0NTk0Nzc3NC4xMS4xLjE3NDU5NDgzNTIuMC4wLjA.&cart=true
+
+
+
+curl --cookie jgi_session=/api/sessions/59175ba19766fd81b026f9d013b0f5b7 --output download.20250429.114025.zip -d "{\"ids\":{\"Phytozome-703\":{\"file_ids\":[\"650deccaf073fe7ce3776d3b\",\"650deccaf073fe7ce3776d37\",\"650decc9f073fe7ce3776d2f\",\"650decc9f073fe7ce3776d2b\",\"650decc9f073fe7ce3776d29\",\"67b5052267ef7b237e865ec3\",\"650decc9f073fe7ce3776d33\",\"650deccaf073fe7ce3776d3f\",\"650decc9f073fe7ce3776d27\",\"650decc9f073fe7ce3776d35\",\"67b5052267ef7b237e865ec5\",\"650deccaf073fe7ce3776d3d\",\"650deccaf073fe7ce3776d41\",\"650decc9f073fe7ce3776d31\",\"650deccaf073fe7ce3776d43\",\"650decc9f073fe7ce3776d25\",\"650decc9f073fe7ce3776d2d\",\"650deccaf073fe7ce3776d39\",\"65370155f073fe7ce3920020\"],\"top_hit\":\"650deccaf073fe7ce3776d3b\"}},\"api_version\":\"2\"}" -H "Content-Type: application/json" https://files-download.jgi.doe.gov/filedownload/
 
