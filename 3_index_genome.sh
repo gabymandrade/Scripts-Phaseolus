fd20a7b0aa358250607e09be65a44d67a9f47f7b
gabymandrade <59860116+gabymandrade@users.noreply.github.com>
2025-04-30 12:16:20 -0600

    Update 3_index_genome.sh

--- 3_index_genome.sh
+++ 3_index_genome.sh
@@ -1,18 +1,26 @@
-
+#!/bin/bash
+#PBS -N alineamiento    #Nombre del trabajo
+#PBS -l nodes=1:ppn=8   #1 nodo, 8 núcleos (processors per node)
+#PBS -l walltime=12:00:00   #Tiempo máximo de ejecución, en este caso 12 horas
+#PBS -l mem=32gb    #Memoria total requerida, en este ejemplo, 32 GB de RAM
+#PBS -o salida.log    #Archivos de salida
+#PBS -e error.log   #Archivo de error
+#PBS -V   #Exporta tus variables de entorno
+#PBS -q long    #Usa la cola "long" (para trabajos largos)
 
 #This script construct the FM-index for the reference genome, what is necessary to run bwa 
 
 
 #Create directory for my output
-mkdir ../data/bwa
-mkdir ../data/bwa/index_genome
+mkdir ../data/2.Ref_genonome/index_genome
 
 
 #Define variables
-ref_gen=../data/ref_genome_kim/GCA_000512255.2_ASM51225v2_genomic.fna.gz	#Path from bin to my reference genome
-prefix=../data/bwa/index_genome_kim/Capsicum_index_kim	#Index database sequences in the FASTA format
-output=../data/bwa/index_genome_kim	  #Path to my output
+ref_gen=../data/2.Ref_genonome/assembly/Pcoccineus_703_v1.0.fa	#Path from bin to my reference genome
+prefix=../data/2.Ref_genonome/index/Pcoccineus_index_unmasked  #Index database sequences in the FASTA format, importante que este descomprimido
+output=../data/2.Ref_genonome/index  #Path to my output
 
 
-#Run BWA with bwtsw algoritm, because **Capsicum** has a large genome
-bwa index -p $prefix -a bwtsw $ref_gen > $output/Capsicum_index_kim
+#Run BWA with bwtsw algoritm, bwtsw está mejor optimizado para genomas eucariotas como plantas. is: no es óptimo para genomas eucariotas
+
+bwa index -p $prefix -a bwtsw $ref_gen > $output
